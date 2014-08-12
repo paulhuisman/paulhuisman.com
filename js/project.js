6024cc101ead58a39677539350c26ce04e314c83
Paul Huisman <paulhuisman88@gmail.com>
2014-08-12 13:33:29 +0200

    mail to link

--- js/project.js
+++ js/project.js
@@ -12,35 +12,40 @@ $(document).ready(function(){
       {image : 'work/indonesia.jpg', proj_id : '3'},
       {image : 'work/thailand2.jpg', proj_id : '4'},
     ]
-});
+  });
 
-calcResolution();
+  $('a#email_me').click(function(){
+    document.location = 'mailto:paulhuisman88@gmail.com';
+    return false;
+  })  
 
-$(window).resize(function(){
   calcResolution();
-});
 
-function calcResolution(){
-  windowWidth = $(window).width();
-  windowHeight = $(window).height();
+  $(window).resize(function(){
+    calcResolution();
+  });
 
-  if(windowWidth <= 360){
-    $("#supersized").hide();
-    $("#supersized-loader").hide();
-  }
-  else if ( windowWidth > 360) {
-    $("#supersized").show();
-    $("#supersized-loader").show();
-  }
+  function calcResolution(){
+    windowWidth = $(window).width();
+    windowHeight = $(window).height();
 
-  $('#prevslide img').css({
-    'max-width' : '65px',
-    'width' :  (windowWidth / 100) * 6.5+'px'
-  }),
+    if(windowWidth <= 360){
+      $("#supersized").hide();
+      $("#supersized-loader").hide();
+    }
+    else if ( windowWidth > 360) {
+      $("#supersized").show();
+      $("#supersized-loader").show();
+    }
 
-  $('#nextslide img').css({
+    $('#prevslide img').css({
       'max-width' : '65px',
       'width' :  (windowWidth / 100) * 6.5+'px'
-  });
-}
+    }),
+
+    $('#nextslide img').css({
+        'max-width' : '65px',
+        'width' :  (windowWidth / 100) * 6.5+'px'
+    });
+  }
 });
