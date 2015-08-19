abd116734428045e8c56c7aa20b7d159634a1496
Paul Huisman <paulhuisman88@gmail.com>
2015-08-19 12:11:26 +0200

    removed project id and moved work around

--- js/project.js
+++ js/project.js
@@ -7,14 +7,14 @@ $(document).ready(function(){
     transition_speed: 1100,
     slide_links: 'blank',
     slides :[
-      {image : 'work/zakynthos-boats.jpg', proj_id : '1'},
-      {image : 'work/doel.jpg', proj_id : '2'},
-      {image : 'work/berlin.jpg', proj_id : '3'},
-      {image : 'work/loveland-light.jpg', proj_id : '4'},
-      {image : 'work/australia.jpg', proj_id : '5'},
-      {image : 'work/thailand.jpg', proj_id : '6'},
-      {image : 'work/indonesia.jpg', proj_id : '7'},
-      {image : 'work/thailand2.jpg', proj_id : '8'},
+      {image : 'work/zakynthos-boats.jpg'},
+      {image : 'work/berlin.jpg'},
+      {image : 'work/loveland-light.jpg'},
+      {image : 'work/australia.jpg'},
+      {image : 'work/thailand.jpg'},
+      {image : 'work/doel.jpg'},
+      {image : 'work/indonesia.jpg'},
+      {image : 'work/thailand2.jpg'},
     ]
   });
 
