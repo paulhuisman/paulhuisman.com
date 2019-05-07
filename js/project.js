fb3c2bff356b0815cd7192ffae3e1b4a7bcfd055
Paul Huisman <paulhuisman88@gmail.com>
2019-05-07 07:29:23 +0400

    Tmp js solution mobile cv-bar

---
+++ js/project.js
@@ -0,0 +1,11 @@
+function hide_cv_bar() {
+  // tmp solution
+  var block = document.getElementsByClassName('experience-overlay-block');
+  block[0].style.width = '8.333vw';
+  var close = document.getElementsByClassName('close');
+  close[0].style.display = 'none';
+  var list_big = document.getElementsByClassName('cv-list-big');
+  list_big[0].style.display = 'none';
+  var list_small = document.getElementsByClassName('cv-list-small');
+  list_small[0].style.display = 'block';
+}
