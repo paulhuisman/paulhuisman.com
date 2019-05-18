412293ca1d7a709447ab2bea8b14ac6e95483b36
Paul Huisman <paulhuisman88@gmail.com>
2019-05-18 16:18:37 +0200

    Fix text

--- js/project.js
+++ js/project.js
@@ -3,11 +3,13 @@ if (!("ontouchstart" in document.documentElement)) {
   document.documentElement.className += "no-touch";
 }
 
+// Toggle CV bar
 var el = document.querySelector('.experience-overlay-block');
 el.onclick = function() {
   el.classList.toggle('experience-overlay-block-toggled-active');
 }
 
+// Send mail link
 var send_mail = document.querySelector('.send-email');
 send_mail.onclick = function() {
   window.location.href = "mailto:paulhuisman88@gmail.com";
