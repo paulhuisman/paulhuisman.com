99a3a837c5b3ac2d2cad5b539c97711ef2689229
Paul Huisman <f.huisman@erasmusmc.nl>
2026-06-15 09:50:31 +0200

    fix: align

--- src/components/SidebarOverlay.tsx
+++ src/components/SidebarOverlay.tsx
@@ -87,7 +87,7 @@ const SidebarOverlay = () => {
           animate={{ opacity: 1 }}
           transition={{ duration: 0.3, ease: 'easeIn' }}
         >
-          <ul className="mt-48 flex h-5 w-screen items-center justify-start text-xs text-white transition-all duration-300 md:mt-30 md:text-sm lg:mt-50 lg:h-auto">
+          <ul className="mt-[282px] flex h-5 w-screen items-center justify-start text-xs text-white transition-all duration-300 md:mt-[210px] md:text-sm lg:mt-[290px] lg:h-auto">
             <li>
               <div className="animate-pulser mr-8 size-3.5 rounded-full bg-yellow-300 lg:size-5"></div>
             </li>
